281412e45d1231a7fbe970221499997d79b44b29
Margaret Alade <alade.margaret@yandex.ru>
2024-08-09 07:42:49 +0300

    Initial Commit

---
+++ Task-3.1-Command/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-3.1-Command
+//
+//  Created by Маргарет  on 09.08.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
