3ece612ffd44658cd3461e8481bd2a82bd1e873c
Margaret Alade <alade.margaret@yandex.ru>
2024-08-10 12:19:41 +0300

    Initial commit

--- Task-3.1-Command/main.cpp
+++ Task-3.1-Command/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class MessageCommand : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::cout << "Get a message: " << message << "\n";
+    }
+};
+
+class FileCommand : public LogCommand {
+public:
+    void print(const std::string& message) override {
+        std::ofstream f;
+        f.open("file.txt", std::fstream::app);
+        if (f.is_open()) {
+            f << message << "\n";
+        }
+        f.close();
+    }
+};
+
+void print(LogCommand& logcommand, std::string message) {
+    logcommand.print(message);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    MessageCommand messagecom;
+    print(messagecom, "Hello");
+    FileCommand filecommand;
+    print(filecommand, "Hello");
+    
     return 0;
 }
